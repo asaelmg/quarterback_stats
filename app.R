daa1d8b0e091bda509f04975458b47123e7a36c5
Oz <51962210+asaelmg@users.noreply.github.com>
2023-09-25 13:19:51 -0600

    Update app.R

--- app.R
+++ app.R
@@ -1,129 +1,172 @@
-# load the required packages
 library(shiny)
-require(shinydashboard)
-library(ggplot2)
+library(shinydashboard)
+library(magrittr)
 library(dplyr)
+library(ggplot2)
+library(rsconnect)
+library(xfun)
+library(plotly)
+library(DT)
+library(stringr)                          
+library(purrr)                            
+library(rlang)                           
+library(r2d3)                            
+library(nycflights13)
+library(knitr)
 
-recommendation <- read.csv('recommendation.csv',stringsAsFactors = F,header=T)
-
-#head(recommendation)
-
-
-#Dashboard header carrying the title of the dashboard
-header <- dashboardHeader(title = "Basic Dashboard")  
-
-#Sidebar content of the dashboard
-sidebar <- dashboardSidebar(
-  sidebarMenu(
-    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-    menuItem("Visit-us", icon = icon("send",lib='glyphicon'), 
-             href = "https://www.salesforce.com")
-  )
-)
+qb_stats <- read.csv("Quarterback_Stats.csv", stringsAsFactors = F,header = TRUE)
+qb_stats$Current_Team <- as.factor((qb_stats$Current_Team))
+qb_stats$Players <- as.factor((qb_stats$Player))
 
 
-frow1 <- fluidRow(
-  valueBoxOutput("value1")
-  ,valueBoxOutput("value2")
-  ,valueBoxOutput("value3")
-)
+ui <- dashboardPage(title = 'Title Goes Here', 
+                    
+                    header <- dashboardHeader(title = "Quarterback Stats Dashboard"), 
+                    
+                    sidebar <- dashboardSidebar(
+                      
+                      width = 250,
+                      sidebarMenu(
+                        menuItem("Dashboard", tabName = "dashboard", icon = icon("home"))
+                                 ),
+                      
+                      selectInput(
+                        inputId = "Players",
+                        label = "Players",
+                        choices = unique(c(qb_stats$Players)),
+                        selected = "Russell Wilson",
+                        selectize = FALSE
+                                 ),
 
-frow2 <- fluidRow(
-  
-  box(
-    title = "Revenue per Account"
-    ,status = "primary"
-    ,solidHeader = TRUE 
-    ,collapsible = TRUE 
-    ,plotOutput("revenuebyPrd", height = "300px")
-  )
-  
-  ,box(
-    title = "Revenue per Product"
-    ,status = "primary"
-    ,solidHeader = TRUE 
-    ,collapsible = TRUE 
-    ,plotOutput("revenuebyRegion", height = "300px")
-  ) 
-  
-)
+                      selectInput(
+                        inputId = "Years",
+                        label = "Years",
+                        choices = unique(c(qb_stats$Year)),
+                        selected = "2023",
+                        selectize = FALSE
+                      ),
+                      
+                      
+                      downloadButton(outputId = "download_data", label = "Download", width = 4)
+                      
+                                               ), 
+                    
+
+                    
+                    # The body of the dashboard
+                    body <- dashboardBody(
+                      tabsetPanel(id = "tabs",
+                                  tabPanel(title = "Main Dashboard",
+                                           value = "page1",
+                                           fluidRow(valueBoxOutput("value1"),
+                                                    valueBoxOutput("value2"),
+                                                    valueBoxOutput("value3")
+                                                   ),
+                                           fluidRow(column(width = 6,plotlyOutput("TouchdownsbyQtr")),
+                                                    column(width = 6,plotlyOutput("YardsbyQtr"))
+                                                   )
+                                           )
+                                  )
+                                         )
+                    , skin='blue'
+                    
+                  )
 
 
 
-# combine the two fluid rows to make the body
-body <- dashboardBody(frow1, frow2)
 
-#completing the ui part with dashboardPage
-ui <- dashboardPage(title = 'This is my Page title', header, sidebar, body, skin='red')
+server <- function(input, output) {
 
-# create the server functions for the dashboard  
-server <- function(input, output) { 
-  
-  #some data manipulation to derive the values of KPI boxes
-  total.revenue <- sum(recommendation$Revenue)
-  sales.account <- recommendation %>% group_by(Account) %>% summarise(value = sum(Revenue)) %>% filter(value==max(value))
-  prof.prod <- recommendation %>% group_by(Product) %>% summarise(value = sum(Revenue)) %>% filter(value==max(value))
-  
-  
-  #creating the valueBoxOutput content
-  output$value1 <- renderValueBox({
-    valueBox(
-      formatC(sales.account$value, format="d", big.mark=',')
-      ,paste('Top Account:',sales.account$Account)
-      ,icon = icon("stats",lib='glyphicon')
-      ,color = "purple")
+
+  base_stats <- reactive({
+    
+      res <- qb_stats %>% 
+      filter(Players == input$Players)
+      if (input$Players != "") res <- filter(res, Players == input$Players)
+      res
     
     
   })
   
   
+  base_data <- reactive({
+    
+    res <- qb_stats %>% 
+      filter(Players == input$Players, Year == input$Years)
+    if (input$Players != "") res <- filter(res, Players == input$Players)
+    res
+    
+  })
+
+  output$value1 <- renderValueBox({
+    
+      base_data() %>%
+      group_by(Players, Year) %>%
+      summarise(value = sum(TD)) %>%
+      pull() %>%
+      as.integer() %>%
+      valueBox(icon = icon("table"), color = "light-blue",subtitle = "Touchdowns") 
+    
+  })
   
+
   output$value2 <- renderValueBox({
     
-    valueBox(
-      formatC(total.revenue, format="d", big.mark=',')
-      ,'Total Expected Revenue'
-      ,icon = icon("usd",lib='glyphicon')
-      ,color = "green")
+      base_data() %>%
+      group_by(Players, Year) %>%
+      summarise(value = sum(Yds)) %>%
+      pull() %>%
+      as.integer() %>%
+      valueBox(icon = icon("table"), color = "purple",subtitle = "Yards") 
     
   })
   
-  
-  
+
   output$value3 <- renderValueBox({
     
-    valueBox(
-      formatC(prof.prod$value, format="d", big.mark=',')
-      ,paste('Top Product:',prof.prod$Product)
-      ,icon = icon("menu-hamburger",lib='glyphicon')
-      ,color = "yellow")
+      base_data() %>%
+      group_by(Players, Year) %>%
+      summarise(value = sum(Sk)) %>%
+      pull() %>%
+      as.integer() %>%
+      valueBox(icon = icon("table"), color = "teal",subtitle = "Sacks") 
     
   })
   
-  #creating the plotOutput content
   
-  output$revenuebyPrd <- renderPlot({
-    ggplot(data = recommendation, 
-           aes(x=Product, y=Revenue, fill=factor(Region))) + 
-      geom_bar(position = "dodge", stat = "identity") + ylab("Revenue (in Euros)") + 
-      xlab("Product") + theme(legend.position="bottom" 
-                              ,plot.title = element_text(size=15, face="bold")) + 
-      ggtitle("Revenue by Product") + labs(fill = "Region")
+  
+
+  output$TouchdownsbyQtr <- renderPlotly({
+
+      ggplot(data = base_stats(), 
+                  aes(x=Year, y=TD, fill=factor(Tm))) +
+        geom_bar(position = "dodge",stat = "identity") + ylab("Touchdowns") + 
+        xlab("Year") + theme(legend.position="bottom", 
+                             plot.title = element_text(size=15, face="bold")) + 
+        ggtitle("Touchdowns by Quarterback") + labs(fill = "Tm")
+      
+         
+                                        })
+
+  
+  output$YardsbyQtr <- renderPlotly({
+    
+    ggplot(data = base_stats(), 
+                aes(x=Year, y=Yds, fill=factor(Tm))) + 
+      geom_bar(position = "dodge",stat = "identity") + ylab("Yards") + 
+      xlab("Year") + theme(legend.position="bottom" 
+                           ,plot.title = element_text(size=15, face="bold")) + 
+      ggtitle("Yards by Quarterback") + labs(fill = "Tm")
+    
   })
   
-  
-  output$revenuebyRegion <- renderPlot({
-    ggplot(data = recommendation, 
-           aes(x=Account, y=Revenue, fill=factor(Region))) + 
-      geom_bar(position = "dodge", stat = "identity") + ylab("Revenue (in Euros)") + 
-      xlab("Account") + theme(legend.position="bottom" 
-                              ,plot.title = element_text(size=15, face="bold")) + 
-      ggtitle("Revenue by Region") + labs(fill = "Region")
-  })
-  
-  
-  
+  output$download_data <- downloadHandler(
+    filename = "download_data.csv",
+    content = function(file) {
+      data <- base_stats()
+      write.csv(data, file, row.names = FALSE)
+    }
+  )
+
 }
-
-
-shinyApp(ui, server)
+  shinyApp(ui, server)
